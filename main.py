371ffd7161fc45cb4012543ec1789bee7aadfa69
Skyler Spaeth <skylerspaeth.scc@me.com>
2025-03-28 20:44:30 -0500

    initial commit of main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import logging, os
+
+from pyhap.accessory import Accessory
+from pyhap.accessory_driver import AccessoryDriver
+from pyhap.const import CATEGORY_DOOR_LOCK
+
+logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
+
+class SchlageLock(Accessory):
+    """Wrapper for dknowles2's pyschlage library"""
+
+    category = CATEGORY_DOOR_LOCK
+
+    def __init__(self, driver, name=os.environ.get('SCHLAGE_HOMEKIT_LOCK_NAME', 'My Lock')):
+        super().__init__(driver, name)
+
+        self.lock_mechanism = self.add_preload_service("LockMechanism")
+        self.lock_target_state = self.lock_mechanism.get_characteristic("LockTargetState")
+        self.lock_current_state = self.lock_mechanism.get_characteristic("LockCurrentState")
+
+        self.lock_target_state.setter_callback = self.lock_changed
+
+    """Callback for when HomeKit requests lock state to change"""
+    def lock_changed(self, value):
+        if value == 1:
+            print("Locking the door...")
+        else:
+            print("Unlocking the door...")
+
+        # Update the current state to reflect the change
+        self.lock_current_state.set_value(value)
+
+def main():
+    driver = AccessoryDriver(port=51826)
+    lock = SchlageLock(driver)
+    driver.add_accessory(accessory=lock)
+    driver.start()
+
+if __name__ == "__main__":
+    main()
